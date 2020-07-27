eb9b9f32b53131ba527a2ff23c96ba1f6084e9b2
liulifenyf <836321388@qq.com>
2020-07-27 11:26:17 +0800

    update

--- Train.py
+++ Train.py
@@ -1,38 +1,36 @@
 from __future__ import absolute_import
 from __future__ import print_function
 import numpy as np
-np.random.seed(7)
 import re
 import os
 import sys
 import random
 import time
 from sklearn.model_selection import train_test_split
-from keras.models import Sequential, model_from_json
-from keras.callbacks import EarlyStopping, ModelCheckpoint
-from keras.layers import Dense, Dropout, Activation, Flatten, Conv1D, MaxPooling1D
+from keras.models import Sequential,model_from_json
+from keras.callbacks import EarlyStopping,ModelCheckpoint 
+from keras.layers import Dense, Dropout, Activation, Flatten,Conv1D, MaxPooling1D
 from keras.optimizers import SGD
 
 np.set_printoptions(threshold=sys.maxsize)
 output = 'output'
-Trained_models = 'Trained_models'
+models='Trained_models'
 
-# create folder to save train result 
+# create a file folder for saving output files
 if not os.path.exists(output):
     os.mkdir(output)
     print('{0} creat successful!'.format(output))
 else:
     print('{0} has been exists.'.format(output))
 
-# create folder to save trained models 
-if not os.path.exists(Trained_models):
-    os.mkdir(Trained_models)
-    print('{0} creat successful!'.format(Trained_models))
+# create a file folder for saving trained models 
+if not os.path.exists(models):
+    os.mkdir(models)
+    print('{0} creat successful!'.format(models))
 else:
-    print('{0} has been exists.'.format(Trained_models))
+    print('{0} has been exists.'.format(models))
 
-
-# make ont-hot matrix
+# Create the ont-hot matrix
 def Matrix(dirs, label_value, num_need):
     files = open(dirs, 'r')
     sample = []
@@ -63,7 +61,7 @@ def Matrix(dirs, label_value, num_need):
         [np.array(sample).shape[0], 1])
 
 
-# create train and test set
+# create training and testing datasets
 def CTTD(pos_sample, pos_label, neg_sample, neg_label):
     X = np.row_stack((pos_sample, neg_sample))
     Y = np.row_stack((pos_label, neg_label))
@@ -72,7 +70,7 @@ def CTTD(pos_sample, pos_label, neg_sample, neg_label):
     return input_train, input_test, input_label_train, input_label_test
 
 
-# create model
+# Model construction
 def Model(filter_num, filter_len):
     model = Sequential()
     model.add(
@@ -92,14 +90,14 @@ def Model(filter_num, filter_len):
     return model
 
 
-def train(pos_file, neg_file, filter_len, num_epoch=100, filter_num=16):
+def train(pos_file,neg_file,filter_len,num_epoch=100,filter_num=16):
     start = time.time()
-    # create file to sumthe process and result
+    # create a file to record training results
     out = open(
         output +
         '/{}-result-{}.txt'.format(re.split('[/.]', pos_file)[-2], filter_len),
         'w')
-    #read file and create positive and negitive data
+    #read files and create positive and negitive datasets
     f = open(pos_file, 'r')
     pos_num = f.read().count('>')
     f.close()
@@ -111,13 +109,13 @@ def train(pos_file, neg_file, filter_len, num_epoch=100, filter_num=16):
                                         num_need=pos_num)
     input_train, input_test, input_label_train, input_label_test = CTTD(
         pos_matrix, pos_label, neg_train, neg_label_train)
-    out.write('The number of train datas: %s\n' % len(input_train))
-    out.write('The number of test datas: %s\n' % len(input_test))
+    out.write('The number of training samples: %s\n' % len(input_train))
+    out.write('The number of testing samples: %s\n' % len(input_test))
     # train and test model
     model = Model(filter_num, filter_len)
     sgd = SGD(lr=0.001, decay=1e-5, momentum=0.9, nesterov=True)
     checkpoint = ModelCheckpoint(
-        filepath=Trained_models +
+        filepath=models +
         '/{}-model-{}.hdf5'.format(re.split('[/.]', pos_file)[-2], filter_len),
         save_best_only=True,
         monitor='val_accuracy',
@@ -131,14 +129,12 @@ def train(pos_file, neg_file, filter_len, num_epoch=100, filter_num=16):
                        batch_size=128,
                        epochs=num_epoch,
                        shuffle=True,
-					   verbose=0,
+                       verbose=0,
                        validation_data=(input_test, input_label_test),
                        callbacks=[checkpoint])
     end = time.time()
-    all_time = end - start
+    training_time = end - start
     val_acc_value = 0
-    epoch_value = 0
-    acc_value = 0
     out.write('epoch\ttrain_loss\ttrain_acc\tval_loss\tval_acc\n')
     for epochs in range(len(result.epoch)):
         if float(result.history['val_accuracy'][epochs]) >= val_acc_value:
@@ -149,26 +145,24 @@ def train(pos_file, neg_file, filter_len, num_epoch=100, filter_num=16):
     out.write('\tepoch: %s\n' % epoch_value)
     out.write('\ttrain_acc: %s\n' % acc_value)
     out.write('\tval_acc: %s\n' % val_acc_value)
-    out.write('\tusing time: %s\n' % all_time)
+    out.write('\tusing time: %s\n' % training_time)
     out.close()
 
 
-# train for best filter length
+# grid search for the optimal filter length
 def grid_train(pos_file, neg_file):
     for filter_len in range(11, 22, 2):
         train(pos_file, neg_file, filter_len=filter_len)
 
 
-
 def main():
+    pos_file = 'Example/ABF2.fasta'
+    neg_file = 'Example/neg.fasta'
+    grid_train(pos_file, neg_file)
 	'''
-		Training example with ABF2 factor, change the 
-	fasta file name following pos_file to train another factor.
-	'''
-	pos_file = 'fasta/ABF2.fasta'
-	neg_file = 'fasta/neg.fasta'
-	grid_train(pos_file, neg_file)
+	Here we take ABF2 as an example to show the training process. For other users, just change pos_file to be your desired fasta file name.
+    '''
 
 
 if __name__ == "__main__":
-    main()
+	main()
